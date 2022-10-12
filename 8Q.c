a55656c4be230e9fb3fec5dab1484fde51c25090
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-10-12 19:16:47 +0530

    Create 8Q.c

---
+++ 8Q.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+   int i,j,a,number[10];
+   printf ("Enter the numbers \n");
+   for (i=0; i<10; ++i)
+      scanf ("%d",&number[i]);
+   for (i=0; i<9; ++i){
+      for (j=i+1; j<10; ++j){
+         if (number[i] < number[j]){
+            a = number[i];
+            number[i] = number[j];
+            number[j] = a;
+         }
+      }
+   }
+   printf ("The 2nd smallest number is = %d\n", number[1]);
+}
